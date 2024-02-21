bd10f9b50d6f455dd6ba5176bb8ab8e57edf8316
bachlx <luongxuanbach21112000@gmail.com>
2024-02-21 14:22:41 +0700

    start project

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="MainLayout" class="w-full fixed z-50">
+        <div id="TopMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
+            <ul>
+                
+            </ul>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    "nuxt-icon",
+    "nuxt-lodash",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@nuxtjs/tailwindcss",
+    // "@nuxtjs/supabase",
+  ],
+  runtimeConfig: {
+    public: {
+      stripePk: process.env.STRIPE_PK_KEY
+    }
+  },
+  build: {
+    transpile: ['pinia-plugin-persistedstate'],
+  },
+  app: {
+    head: {
+      script: [
+        { src: 'https://js.stripe.com/v3/', defer: true}
+      ]
+    }
+  }
+});
